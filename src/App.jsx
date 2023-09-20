aa5fe4963d3a91884690aa7e87cc5f9de13b51a2
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2023-09-20 18:28:04 -0300

    mudança pra react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home';
+
+
+function App() {
+  // window.alert(`${data.decks[0].name}`)
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home(){
+    return <h1>Eu sou a HOME</h1>
+}
