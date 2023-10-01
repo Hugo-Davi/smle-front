5fb5b90c584df661e49110521d1d651acd38c891
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2023-10-01 17:43:11 -0300

    Study page

--- src/pages/StudyPage.jsx
+++ src/pages/StudyPage.jsx
@@ -6,6 +6,34 @@ const StudyPage = () => {
     const { id } = useParams();
     const [ cards, setCards ] = useState();
 
+    const [ isAnswered, setIsAnswered ] = useState(false);
+
+    const [ reviewInput, setReviewInput ] = useState();
+
+    useEffect(() => {
+        switch(reviewInput){
+            case 1:
+                console.log('Passou a carta');
+                // let passedCards = cards;
+                // passedCards = passedCards.push(passedCards.shift())
+                // setCards(passedCards);
+                break;
+            case 2:
+                console.log('Hard');
+                break;
+            case 3:
+                console.log('Ok');
+                break;
+            case 4:
+                console.log('Easy')
+                break;
+            default:
+                return;
+        }
+        setIsAnswered(false);
+        setReviewInput(0);
+    }, [reviewInput])
+
     useEffect(() => {
         async function getCards() {
             try {
@@ -21,15 +49,23 @@ const StudyPage = () => {
         return <div>carregando </div>
     }
     console.log(cards)
+
     return (
         <>
-            <div className=''>
-                {   
-
-                    cards.map((element) => {
-                        return <div key={element._id}>{element.front}</div>
-                    })
-                }
+            <div className='bg-baltic-900 m-5'>
+                <div className='text-center' onClick={() => setIsAnswered(true)}>
+                    {cards[0].front}
+                </div>
+                <div className={isAnswered ? `block` : `hidden`}>
+                    <br />
+                    <p className='text-center'>{cards[0].back}</p>
+                    <div className='flex justify-center align-middle'>
+                        <button className='bg-slate-500 p-3 mr-3' onClick={() => setReviewInput(1)}>Pass</button>
+                        <button className='bg-fuchsia-500 p-3 text-baltic-900' onClick={() => setReviewInput(2)}>Hard</button>
+                        <button className='bg-yellow-300 p-3 text-baltic-900' onClick={() => setReviewInput(3)}>Ok</button>
+                        <button className='bg-green-400 p-3 text-baltic-900' onClick={() => setReviewInput(4)}>Easy</button>
+                    </div>
+                </div>
             </div>
         </>
     )
