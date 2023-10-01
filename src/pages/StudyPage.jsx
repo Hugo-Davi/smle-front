1086490f68041482d1787f00fb5b1ca2a96f72a7
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2023-09-30 22:08:00 -0300

    card useState

--- src/pages/StudyPage.jsx
+++ src/pages/StudyPage.jsx
@@ -5,12 +5,14 @@ import { useParams } from 'react-router-dom';
 const StudyPage = () => {
     const { id } = useParams();
     const [ deck, setDeck ] = useState(null);
+    const [ cards, setCards ] = useState([]);
 
     useEffect(() => {
         async function getCards() {
             try {
                 const response = await deckService.get(id);
                 setDeck(response);
+                setCards(response.deck)
             } catch (error) {
                 console.log(error);
             }
