8df4ff25af56cc72a6fb8ab6d095558d35ecc6b9
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2023-10-18 11:52:43 -0300

    REVIEW SYSTEM

--- src/pages/StudyPage.jsx
+++ src/pages/StudyPage.jsx
@@ -5,27 +5,41 @@ import { useParams } from 'react-router-dom';
 const StudyPage = () => {
     const { id } = useParams();
     const [ cards, setCards ] = useState();
+    const [ card, setCard ] = useState(null);
 
     const [ isAnswered, setIsAnswered ] = useState(false);
 
     const [ reviewInput, setReviewInput ] = useState();
 
+    function updateDeck(n){
+        console.log(cards);
+        const newDeck = cards.map( (deckCard) => {
+            if(deckCard._id === card._id) {
+                let newDate = new Date;
+                newDate.setMinutes(newDate.getMinutes() + n);
+                deckCard.nextReview = newDate.toISOString();
+            }
+            return deckCard;
+        } );
+        setCards(newDeck);
+    }
+
     useEffect(() => {
         switch(reviewInput){
             case 1:
                 console.log('Passou a carta');
-                // let passedCards = cards;
-                // passedCards = passedCards.push(passedCards.shift())
-                // setCards(passedCards);
                 break;
             case 2:
                 console.log('Hard');
+                updateDeck(1);
                 break;
             case 3:
                 console.log('Ok');
+                updateDeck(100)
                 break;
             case 4:
                 console.log('Easy')
+                updateDeck(1000);
                 break;
             default:
                 return;
@@ -40,30 +54,44 @@ const StudyPage = () => {
                 const response = await deckService.get(id);
                 setCards(response.body.cards.filter((element) => Date.parse(element.nextReview) < Date.now()));
             } catch (error) {
-                console.log(error);
+                return <div>Carregando</div>
             }
         }
         getCards();
     }, [id])
+
+    useEffect(() => {
+        async function getCard() {
+            try {
+                setCard(cards.find( (card) => Date.parse(card.nextReview) < Date.now() ));
+            } catch (error) {
+                return <div>Carregando</div>
+            }
+        }
+        getCard();
+    }, [cards])
+
     if(!cards){
         return <div>carregando </div>
     }
-    console.log(cards)
+
 
     return (
         <>
-            <div className='bg-baltic-900 m-5'>
+            <div className='m-5 bg-baltic-900'>
                 <div className='text-center' onClick={() => setIsAnswered(true)}>
-                    {cards[0].front}
+                    {card ? card.front : 'aguarde'}
                 </div>
                 <div className={isAnswered ? `block` : `hidden`}>
                     <br />
-                    <p className='text-center'>{cards[0].back}</p>
+                    <p className='text-center'>
+                        {card ? card.back : 'aguarde'}
+                    </p>
                     <div className='flex justify-center align-middle'>
-                        <button className='bg-slate-500 p-3 mr-3' onClick={() => setReviewInput(1)}>Pass</button>
-                        <button className='bg-fuchsia-500 p-3 text-baltic-900' onClick={() => setReviewInput(2)}>Hard</button>
-                        <button className='bg-yellow-300 p-3 text-baltic-900' onClick={() => setReviewInput(3)}>Ok</button>
-                        <button className='bg-green-400 p-3 text-baltic-900' onClick={() => setReviewInput(4)}>Easy</button>
+                        <button className='p-3 mr-3 bg-slate-500' onClick={() => setReviewInput(1)}>Pass</button>
+                        <button className='p-3 bg-fuchsia-500 text-baltic-900' onClick={() => setReviewInput(2)}>Hard</button>
+                        <button className='p-3 bg-yellow-300 text-baltic-900' onClick={() => setReviewInput(3)}>Ok</button>
+                        <button className='p-3 bg-green-400 text-baltic-900' onClick={() => setReviewInput(4)}>Easy</button>
                     </div>
                 </div>
             </div>
